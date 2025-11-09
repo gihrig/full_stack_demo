c013517b49f6d22948b8d623c3e465871704f864
gihrig <glen@arkadias.com>
2025-11-09 02:34:33 -0800

    style(todo.rs, etc): introduce tailwind

--- src/todo.rs
+++ src/todo.rs
@@ -177,7 +177,7 @@ pub fn Todos() -> impl IntoView {
         <div>
             <Transition fallback=move || view! { <p>"Loading..."</p> }>
                 <ErrorBoundary fallback=|errors| view! { <ErrorTemplate errors /> }>
-                    <ul class="text-red-500 bg-blue-200">
+                    <ul class="text-red-500 bg-blue-200 w-96 mx-auto">
                         {existing_todos}
                         {move || {
                             submissions
@@ -193,7 +193,6 @@ pub fn Todos() -> impl IntoView {
                                 })
                                 .collect::<Vec<_>>()
                         }}
-
                     </ul>
                 </ErrorBoundary>
             </Transition>
